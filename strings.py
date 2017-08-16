e26a5def25449fb2269274e9daac1bc3e8d98d73
jack-evan <jack.evan@live.com>
2017-08-16 16:16:26 -0600

    adding strings

---
+++ strings.py
@@ -0,0 +1,9 @@
+str = "Hello Python!"
+print str
+print str[0]
+print str[2:5] #prints from 3 character to 5th
+print str[2:] #prints from 3 character on..
+print str * 2 #print string two times
+print str + "TEST" #print concatenated string
+
+
