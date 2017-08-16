f484c5093bd04a58e956581fe8da470c0f2ba7fd
jack-evan <jack.evan@live.com>
2017-08-16 16:24:43 -0600

    adding lists

---
+++ lists.py
@@ -0,0 +1,7 @@
+#this is a list
+list = ["one","two",123,3.45,"wayne",90.4]
+
+print list #prints complete list
+print list[0] #prints first element (one)
+print list[1:3] #prints second elment to third
+print list[2:] #prints from third element on
