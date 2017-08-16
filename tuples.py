2a3dd14fab87cfaa78bc93624251c2534033d90e
jack-evan <jack.evan@live.com>
2017-08-16 16:29:28 -0600

    adding tuples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+#this is a tuple
+someval = ("one","two",345,45.5,"summer")
+
+#print tuple
+print someval #prints tuple
+print someval[0] #prints first element of the tuple
+print someval[2:] #prints third element and on
+print someval * 2 #prints tuple twice
+
