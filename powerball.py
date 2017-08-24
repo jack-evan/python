3af6b2c4239afdbe1c62d470016ecef12cc9f80c
jack-evan <jack.evan@live.com>
2017-08-24 15:16:42 -0600

    adding powerball generator

---
+++ powerball.py
@@ -0,0 +1,13 @@
+import random
+
+#get numbers
+for i in range(5):
+  rana = random.randint(1,69)
+  print rana,
+
+
+#get powerball
+for n in range(1):
+  ranb = random.randint(1,26)
+  print '(' + str(ranb) +')',
+
