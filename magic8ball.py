a0a7c1f7d2f80c574c9504a4e5de1152eedae820
jack-evan <jack.evan@live.com>
2017-09-15 10:39:32 -0600

    changed question

--- magic8ball.py
+++ magic8ball.py
@@ -4,7 +4,7 @@ answers = ['It is certain','It is decidedly so','Without a doubt','Yes definitel
 'Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Do not count on it','My reply is no',
 'My sources say no','Outlook not so good','Very doubtful']
 
-question = raw_input('What would you like to know ')
+question = raw_input('Type a yes or now question ')
 
 
 random_index = randrange(0,len(answers))
